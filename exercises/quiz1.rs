b0698c999e2c2e3fad1e88144d7bbf0bde18b8ae
Henry Wetton <henrywetton@outlook.com>
2024-01-11 13:48:11 +0100

    Complete quiz 1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,12 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num_apples: u32) -> u32 {
+    let price_per_apple = if num_apples > 40 { 1 } else { 2 };
+    price_per_apple * num_apples
+}
 
 // Don't modify this function!
 #[test]
